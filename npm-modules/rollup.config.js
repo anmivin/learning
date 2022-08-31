e7eb8677af9bb23d103542bed0c2698b8dbce989
anmivin <anmivin@yandex.ru>
2022-08-31 19:20:28 +0300

    в общем, собрала только чисто хуки

--- npm-modules/rollup.config.js
+++ npm-modules/rollup.config.js
@@ -1,4 +1,45 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import external from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+const rollupConfig = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "react-lib",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: packageJson.typings, format: "esm" }],
+
+    plugins: [dts()],
+  },
+];
+
+export default rollupConfig;
+
+/* import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
@@ -30,4 +71,4 @@ const rollupConfig = {
   ],
 };
 
-export default rollupConfig;
+export default rollupConfig; */
---
+++ npm-modules/src/react-visibility/index.ts
@@ -0,0 +1,1 @@
+export { usePageVisibility } from "./usePageVisability";
